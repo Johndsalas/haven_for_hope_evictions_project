e8f9086263b671c76872107b874ebc8d58d05513
John Salas <johndsalas@gmail.com>
2025-06-18 15:12:22 -0500

    updating prep files

--- evictions_prep.py
+++ evictions_prep.py
@@ -1,11 +1,6 @@
 ''' Contains code for preparing evictions data'''
 
 import pandas as pd
-from thefuzz import fuzz
-
-import warnings
-warnings.filterwarnings("ignore")
-
 
 def get_prepared_eviction_data():
     '''Prepare eviction data for project'''
@@ -15,71 +10,57 @@ def get_prepared_eviction_data():
     
     # get relevant columns and rename for clarity
     df = df[['CaseNumber',        # used to distinguish unique cases
-             'CaseFileDate',      # used for case date
              'JUDGMENT_DT',       # used for date case was ruled on
-             'DispositionDate',   # used for date of disposition
-             'CITY_NAME',         # used for city location
              'POSTAL_CD',         # used for zip code
-             'Disposition',        # used to determine if case was dismissed
-             ]]   
+             'Disposition']]      # used to determine result of case
+
 
     df = df.rename(columns={'CaseNumber'      : 'case_number',
-                            'CaseFileDate'    : 'file_date',
                             'JUDGMENT_DT'     : 'judgement_date',
-                            'DispositionDate' : 'disposition_date',
-                            'CITY_NAME'       : 'city',
-                            'POSTAL_CD'       : 'zip_code',
+                            'POSTAL_CD'       : 'zips',
                             'Disposition'     : 'disposition'})
-    
-    # adjust values in cities for misspellings of san antonio
-    df['city'] = df.city.apply(fuzzy_sa)
 
-    # remove cities not in list of acceptaple cities
-    cities = ['san antonio']
-
-    df = df[df.city.isin(cities)]
-
+    # drop rows with zip codes not in
     # get first five digits of values in zip_code column
-    df['zip_code'] = df.zip_code.apply(lambda x : str(x)[:5])
+    df['zips'] = df.zips.apply(lambda x : str(x)[:5])
 
-    # remove duplicate case numbers keeping the latest according to the disposition date
-    df = df.sort_values(by='disposition_date',ascending=False).reset_index(drop=True)
+    # list of zips in San Antonio
+    sa_zips = ['78201', '78202', '78203', '78204', '78205', 
+               '78206', '78207', '78208', '78209', '78210', 
+               '78211', '78212', '78213', '78214', '78215', 
+               '78216', '78217', '78218', '78219', '78220', 
+               '78221', '78222', '78223', '78224', '78225', 
+               '78226', '78227', '78228', '78229', '78230', 
+               '78231', '78232', '78233', '78234', '78235', 
+               '78236', '78237', '78238', '78239', '78240', 
+               '78241', '78242', '78243', '78244', '78245', 
+               '78246', '78247', '78248', '78249', '78250', 
+               '78251', '78252', '78253', '78254', '78255', 
+               '78256', '78257', '78258', '78259', '78260', 
+               '78261', '78262', '78263', '78264', '78265', 
+               '78266', '78268', '78269', '78270', '78275', 
+               '78278', '78279', '78280', '78283', '78284', 
+               '78285', '78286', '78287', '78288', '78289', 
+               '78291', '78292', '78293', '78294', '78295', 
+               '78296', '78297', '78298', '78299']
 
-    df = df.drop_duplicates(subset='case_number', keep='first').reset_index(drop=True)
+    # keep only rows that have a zip code in sa_zips
+    df = df[df.zips.isin(sa_zips)]
     
-    # removing rows where disposition is not likely to result in eviction
+    # drop rows with duplicate case numbers
+    # sort values in descending order
+    df = df.sort_values(by='judgement_date',ascending=False).reset_index(drop=True)
+
+    # drop rows with duplicate case numbers keeping only the first
+    df = df.drop_duplicates(subset='case_number', keep='first').reset_index(drop=True)
+
+    # drop rows where disposition is not likely to result in eviction
     evict = ['Default Judgments (OCA)',
              'Judgment for Plaintiff (OCA)']
 
     df = df[df.disposition.isin(evict)]
     
-    # remove columns used for filtering
-    df = df[['case_number',
-             'disposition_date',
-             'zip_code']].reset_index(drop=True)
-    
-    df.to_excel('evictions_prepared.xlsx')
-    
-    
-def fuzzy_sa(value):
-    ''' takes in a pandas value and returns san antonio if string is a 90 or higher ratio to san antonio
-        otherwise returns lower cased version of the original string'''
-    
-    value = str(value).lower()
-    
-    sa1 = 'san antonio'
-    sa2 = 'sa'
-    
-    ratio1 = fuzz.ratio(sa1,value)
-    ratio2 = fuzz.ratio(sa2,value)
-    
-    if (ratio1 >= 80) or (ratio2 >= 60):
-        
-        return 'san antonio'
-    
-    else:
-        
-        return value
+    df.to_excel('evictions_prep.xlsx')
     
 
 if __name__ == '__main__':
